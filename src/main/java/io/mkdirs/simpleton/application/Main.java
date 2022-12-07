dc627fdbf86dcb6ae255bb0f601d3c168323ba48
Hafez Ben-Abdillah MADI <hafezbenabdillah.madi@gmail.com>
2022-12-07 19:32:27 +0100

    Added run file

--- src/main/java/io/mkdirs/simpleton/application/Main.java
+++ src/main/java/io/mkdirs/simpleton/application/Main.java
@@ -4,7 +4,7 @@ import io.mkdirs.simpleton.evaluator.ASTNode;
 import io.mkdirs.simpleton.model.token.Token;
 import io.mkdirs.simpleton.result.Result;
 
-import java.util.Arrays;
+import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +13,47 @@ public class Main {
     public static void main(String[] args){
         if(args.length == 0)
             shell();
+        else if(args.length == 1)
+            runFile(args[0]);
+    }
+
+    private static void runFile(String path){
+        File f = new File(path);
+
+        if(!f.exists()){
+            System.err.println("The file "+f.getAbsolutePath()+" doesn't exist");
+            return;
+        }
+
+        if(!f.isFile()){
+            System.err.println("You must provide a file !");
+            return;
+        }
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(f))) {
+            Simpleton simpleton = new Simpleton();
+            String text = "";
+            for(String line : reader.lines().toList()){
+                text += line+"\n";
+            }
+            Result<List<Token>> tokensRes = simpleton.buildTokens(text);
+            if(tokensRes.isFailure()){
+                System.err.println(tokensRes.getMessage());
+                return;
+            }
+
+            Result<List<ASTNode>> nodesRes = simpleton.buildTrees(tokensRes.get());
+
+            if(nodesRes.isFailure()){
+                System.err.println(nodesRes.getMessage());
+                return;
+            }
+
+            simpleton.execute(nodesRes.get());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 
     private static void shell(){
